4c09fdfa6efad060518875de3d6ad5bcf74bf41d
devlucasalmd <lucas.amaral.almd@gmail.com>
2024-05-22 16:55:22 -0300

    feat: cod sem robo sem sensores

--- files/led2/led2.ino
+++ files/led2/led2.ino
@@ -1,27 +1,21 @@
-int intervalo1; //Variavel Global
-int intervalo2; //Variavel Global
+#define PinLed 7 //Entrada digital do Arduino conectado ao LED
+#define PinSensor 8 // Entrada digital do Arduino conectado ao pino OUT do Sensor 
+int LeituraSensor; //Váriavel que irá armazenar os valores de leitura do sensor
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  intervalo1 = 500;
-  intervalo2 = 1000;
+  Serial.begin(9600); //Inicializa a comunicação serial
+  pinMode(PinSensor, INPUT); // Declara o pino do sensor como entrada (INPUT)
+  pinMode(PinLed, OUTPUT); // Declara o pino do led como saída (OUT)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //int intervalo1; Variavel Local
-  //int intervalo2; Variavel Local
-  //intervalo1 = 500;
-  //intervalo2 = 1000;
-  
-  digitalWrite(10, HIGH);  
-  digitalWrite(9, LOW);
-  delay(intervalo1);
-
-  digitalWrite(10, LOW);  
-  digitalWrite(9, HIGH);
-  delay(intervalo2);
-
+  LeituraSensor = digitalRead(PinSensor); //Leitura de dados do sensor
+  Serial.print("Leitura do sensor: ");
+  Serial.println(LeituraSensor);
+  if (LeituraSensor == LOW) { //Se a leitura do sensor for igual a LOW..
+    digitalWrite(PinLed, HIGH); // Acende o LED
+  }
+  else { // Se o valor de leitura do sensor NÃO for igual a LOW
+    digitalWrite(PinLed, LOW); // Apaga o LED
+  }
 }
